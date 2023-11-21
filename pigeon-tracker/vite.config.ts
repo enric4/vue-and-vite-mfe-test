a35e5057c11fc54beea4ecc79fab6d407f178562
enrica.calogero <enrica.calogero@consulting-for.edenred.com>
2023-11-21 23:22:20 +0100

    fix

--- pigeon-tracker/vite.config.ts
+++ pigeon-tracker/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import federation from "@originjs/vite-plugin-federation";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 const APPLICATION_PORT = 5005;
 
@@ -24,6 +25,12 @@ export default defineConfig({
     port: APPLICATION_PORT,
   },
   plugins: [
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: "__tla",
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: i => `__tla_${i}`
+    }),
     vue(),
     vueJsx(),
     federation({
